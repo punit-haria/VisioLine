058d12ba2adf4f3e22e7494b67d410a62b04362a
Justin Chu <justinchu1989@gmail.com>
2013-10-27 14:57:25 -0700

    bugfix

--- src/data/RepoFile.java
+++ src/data/RepoFile.java
@@ -3,6 +3,7 @@ package data;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.Set;
 
 import org.eclipse.jgit.api.BlameCommand;
 import org.eclipse.jgit.api.errors.GitAPIException;
@@ -10,20 +11,25 @@ import org.eclipse.jgit.blame.BlameResult;
 import org.eclipse.jgit.diff.RawText;
 import org.eclipse.jgit.errors.AmbiguousObjectException;
 import org.eclipse.jgit.errors.IncorrectObjectTypeException;
+import org.eclipse.jgit.errors.MissingObjectException;
 import org.eclipse.jgit.errors.RevisionSyntaxException;
 import org.eclipse.jgit.internal.storage.file.FileRepository;
+import org.eclipse.jgit.lib.PersonIdent;
 import org.eclipse.jgit.revwalk.RevCommit;
+import org.eclipse.jgit.revwalk.RevWalk;
+import org.eclipse.jgit.treewalk.filter.PathFilter;
 
 public class RepoFile implements Iterable<Line> {
 	
 	private String fileName;
 	private String commitId;
+	private Set<String> preCommitIds;
 	
 	//change to whatever data structure is most suitable
 	private ArrayList<Line> lines;
 	
 	/*
-	 * @param name is a the file's relative path
+	 * @param name is a the files relative path
 	 * @param commitId must be a commit within this file's history
 	 * otherwise this function will result in an error.
 	 */
@@ -32,23 +38,31 @@ public class RepoFile implements Iterable<Line> {
 		this.fileName = name;
 		this.commitId = commitId;
 		
+		//create list of line objects
+		lines = populateLineInfo(repo);
+	}	
+	
+	/*
+	 * Populates line object. 
+	 */
+	private ArrayList<Line> populateLineInfo(FileRepository repo) throws RevisionSyntaxException, MissingObjectException, IncorrectObjectTypeException, AmbiguousObjectException, IOException, GitAPIException
+	{
 		//obtain blame information
 		BlameCommand blamer = new BlameCommand(repo);
 		blamer.setStartCommit(repo.resolve(commitId));
-		blamer.setFilePath(name);
-		lines = populateLineInfo(blamer.call());
-	}	
-	
-	private ArrayList<Line> populateLineInfo(BlameResult blame)
-	{
-		RawText result = blame.getResultContents();
+		blamer.setFilePath(fileName);
+		
+		BlameResult blame = blamer.call();
+	    
+	    RawText result = blame.getResultContents();
 		int numberOfLines = result.size();
 		ArrayList<Line> lines = new ArrayList<Line>(numberOfLines);
 		
 		for(int i = 0; i < numberOfLines; ++i)
 		{
 			RevCommit currentCommit = blame.getSourceCommit(i);
-			lines.add(new Line(currentCommit.getName(), currentCommit.getName(), i,
+			PersonIdent currentAuthor = blame.getSourceAuthor(i);
+			lines.add(new Line(currentCommit.getName(), currentAuthor.getName(), i,
 					result.getString(i)));
 		}
 		
