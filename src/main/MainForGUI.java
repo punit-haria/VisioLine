62c9d3d7624971c8c1654680efc7e9ec834bd978
Mark Ang <markjedricang@gmail.com>
2013-10-29 00:09:07 -0700

    First attempt of making a container frame for the visualizer

---
+++ src/main/MainForGUI.java
@@ -0,0 +1,46 @@
+package main;
+
+import java.awt.*;
+
+public class MainForGUI extends Frame {
+	
+	Panel panel;
+	Button b1,b2;
+	    public MainForGUI()
+	    {
+	    
+	    // Set frame properties
+	    setTitle("MainForGUI"); // Set the title
+	    setSize(400,400); // Set size to the frame
+	    setLayout(new FlowLayout()); // Set the layout
+	    setVisible(true); // Make the frame visible
+	    setLocationRelativeTo(null);  // Center the frame
+	    
+	    // Create the panel
+	    panel=new Panel();
+	    
+	    // Set panel background
+	    panel.setBackground(Color.gray);
+	    
+	    // Create buttons
+	    b1=new Button(); // Create a button with default constructor
+	    b1.setLabel("I am button 1"); // Set the text for button
+	    
+	    b2=new Button("Button 2"); // Create a button with sample text
+	    b2.setBackground(Color.lightGray); // Set the background to the button
+	    
+	    // Add the buttons to the panel
+	    panel.add(b1);
+	    panel.add(b2);
+	    
+	    // Add the panel to the frame
+	    add(panel);
+	    
+	    }
+	    
+	    public static void main(String args[])
+	    {
+	    new MainForGUI();
+	    }
+
+}
