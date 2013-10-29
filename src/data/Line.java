4d10d80e271ab91e39f1f25049b1cae94b4f143d
Justin Chu <justinchu1989@gmail.com>
2013-10-29 14:07:29 -0700

    Adding times changed variable to

--- src/data/Line.java
+++ src/data/Line.java
@@ -1,20 +1,20 @@
 package data;
 
-import java.util.Date;
-
 public class Line {
 	
 	private String commitId;
 	private String author;
 	private int lineNumber;
 	private String lineValue;
+	private int timesChanged;
 	
 	public Line(String commitId, String author,
 			int lineNumber, String lineValue){
 		this.commitId = commitId;
 		this.author = author;
 		this.lineNumber = lineNumber;
-		this.lineValue = lineValue;		
+		this.lineValue = lineValue;
+		timesChanged = 0;
 	}
 	
 	public String getCommitId() {
@@ -31,6 +31,10 @@ public class Line {
 
 	public String getLineValue() {
 		return lineValue;
+	}
+	
+	public int getTimesChanged() {
+		return timesChanged;
 	}
 	
 }
