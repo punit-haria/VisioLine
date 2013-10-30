71517243a7ac332d1649d97e26033f28bea04435
unknown <punitshah163@gmail.com>
2013-10-30 15:07:04 -0700

    Updated mainForGUI to include visualizer

--- src/main/MainForGUI.java
+++ src/main/MainForGUI.java
@@ -6,118 +6,121 @@ import java.awt.event.WindowEvent;
 import java.io.File;
 import java.io.IOException;
 import java.util.HashSet;
-import java.util.Iterator;
 import java.util.LinkedList;
 
 import javax.swing.JFileChooser;
 
-import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.api.errors.GitAPIException;
 import org.eclipse.jgit.errors.RevisionSyntaxException;
-import org.eclipse.jgit.lib.ObjectId;
-import org.eclipse.jgit.lib.Repository;
 import org.eclipse.jgit.revwalk.RevCommit;
-import org.eclipse.jgit.revwalk.RevTree;
-import org.eclipse.jgit.revwalk.RevWalk;
-import org.eclipse.jgit.treewalk.TreeWalk;
-import org.eclipse.jgit.treewalk.filter.PathFilter;
 
-import data.Line;
-import data.RepoFile;
-import data.RepoFileManager;
 import visual.primary.Visualizer;
 
+import data.RepoFile;
+import data.RepoFileManager;
+
 public class MainForGUI extends Frame {
-	
+
 	private static String gitPath;
 	Panel panel;
 	Button b1,b2;
-	    public MainForGUI()
-	    {
-	    
-	    // Set frame properties
-	    setTitle("MainForGUI"); // Set the title
-	    setSize(400,400); // Set size to the frame
-	    setLayout(new FlowLayout()); // Set the layout
-	    setVisible(true); // Make the frame visible
-	    setLocationRelativeTo(null);  // Center the frame
-	    
-	    // Create the panel
-	    panel=new Panel();
-	    
-	    // Set panel background
-	    panel.setBackground(Color.gray); 	 	
-	    
-	    // Create buttons
-	    b1=new Button(); // Create a button with default constructor
-	    b1.setLabel("I am button 1"); // Set the text for button
-	    
-	    b2=new Button("Button 2"); // Create a button with sample text
-	    b2.setBackground(Color.lightGray); // Set the background to the button
-	    
-	    // Add the buttons to the panel
-	    panel.add(b1);
-	    panel.add(b2);
-	    
-	    // Add the panel to the frame
-	    add(panel);
-	    
-	    addWindowListener(new WindowAdapter(){
-	    	  public void windowClosing(WindowEvent we)
-	    	  {
-	    	  System.exit(0);
-	    	  }
-	    	 });
-	    	 }
-	    
-	    
-	    
-	    public static void main(String args[])
-	    {
-			 JFileChooser chooser = new JFileChooser();
-			    chooser.setDialogTitle("Select target directory");
-			    chooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
-			    int returnVal = chooser.showOpenDialog(new Frame());
-			    if(returnVal == JFileChooser.APPROVE_OPTION) {
-			    File myFile = chooser.getSelectedFile();
-			    
-			    gitPath=myFile.getAbsolutePath();
-			    }
-			    try {
-			    	
-					RepoFileManager rfm=new RepoFileManager(gitPath);
-					 
-					 LinkedList<String> files=rfm.getRelevantPathofFilesByExetension("java");
-					  
-					 Iterable<RevCommit> commits=rfm.getCommitList();
-				
-					 int i=0;
-	 
-					 //for(RevCommit commit:commits){
-						 for(String s:files){
-							System.out.println("the interation: "+i++);
-							 HashSet<String> commitlastIds= RepoFileManager.getAlteringCommitIDs(rfm.getRepo(), s);
-						 
-						for(String commit:commitlastIds) {
-							System.out.println("this is th file relevant path "+s);
-								System.out.println("this commitId is "+ commit); 
-						 RepoFile rf=new RepoFile(rfm.getRepo(), s, commit );
-							 /*Iterator<Line> lines= rf.iterator();
+	public MainForGUI()
+	{
+
+		// Set frame properties
+		setTitle("MainForGUI"); // Set the title
+		setSize(400,400); // Set size to the frame
+		//layout
+		this.setLayout(new BorderLayout());
+		setVisible(true); // Make the frame visible
+		setLocationRelativeTo(null);  // Center the frame
+
+		// Create the panel
+		panel=new Panel();
+
+		// Set panel background
+		panel.setBackground(Color.gray); 	 	
+
+		// Create buttons
+		b1=new Button(); // Create a button with default constructor
+		b1.setLabel("I am button 1"); // Set the text for button
+
+		b2=new Button("Button 2"); // Create a button with sample text
+		b2.setBackground(Color.lightGray); // Set the background to the button
+
+		// Add the buttons to the panel
+		panel.add(b1);
+		panel.add(b2);
+
+		// Add the panel to the frame
+		this.add(panel,BorderLayout.NORTH);
+
+		addWindowListener(new WindowAdapter(){
+			public void windowClosing(WindowEvent we)
+			{
+				System.exit(0);
+			}
+		});
+
+		//Test visualization
+		Visualizer vis = new Visualizer();
+		this.add(vis,BorderLayout.CENTER);
+		vis.init();
+	}
+
+
+
+	public static void main(String args[])
+	{
+		JFileChooser chooser = new JFileChooser();
+		chooser.setDialogTitle("Select target directory");
+		chooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+		int returnVal = chooser.showOpenDialog(new Frame());
+		if(returnVal == JFileChooser.APPROVE_OPTION) {
+			File myFile = chooser.getSelectedFile();
+
+			gitPath=myFile.getAbsolutePath();
+		}
+		try {
+
+			RepoFileManager rfm=new RepoFileManager(gitPath);
+
+			LinkedList<String> files=rfm.getRelevantPathofFilesByExetension("java");
+
+			Iterable<RevCommit> commits=rfm.getCommitList();
+
+			int i=0;
+
+			//visualizer
+			ArrayList<RepoFile> fileList = new ArrayList<RepoFile>();
+			
+			//for(RevCommit commit:commits){
+			for(String s:files){
+				System.out.println("the interation: "+i++);
+				HashSet<String> commitlastIds= RepoFileManager.getAlteringCommitIDs(rfm.getRepo(), s);
+
+				for(String commit:commitlastIds) {
+					System.out.println("this is th file relevant path "+s);
+					System.out.println("this commitId is "+ commit); 
+					RepoFile rf=new RepoFile(rfm.getRepo(), s, commit );
+					/*Iterator<Line> lines= rf.iterator();
 								while(lines.hasNext()){
 									Line line=lines.next();
 									System.out.println(line.getLineNumber()+line.getAuthor()+line.getCommitId()+line.getLineValue());
 								} */
-							 
-						}
-						
-						 
-					 }
-				} catch (RevisionSyntaxException | GitAPIException | IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
+					
+					//visualizer code
+					
 				}
-	    	 
-	    new MainForGUI();
-	    }
+
+
+			}
+		} catch (RevisionSyntaxException | GitAPIException | IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		new MainForGUI();
+	}
 
 }
