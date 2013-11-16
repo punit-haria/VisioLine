4b0f0591585c3def4bc4d2a3a7c4f29768221380
Justin Chu <justinchu1989@gmail.com>
2013-11-16 11:51:44 -0800

    Added line type constants, removed times changed object.

--- src/data/Line.java
+++ src/data/Line.java
@@ -6,10 +6,15 @@ import java.util.Iterator;
 
 public class Line implements Serializable {
 	
+	//variable for determining type
+	static int IMPORT_TYPE = 1;
+	static int WHITESPACE_TYPE = 2;
+	static int FUNCTION_TYPE = 3;
+	static int COMMENT_TYPE = 4;
+	
 	private int lineNumber;
 	private String lineValue;
-	private int timesChanged;
-	private String codeType;
+	private int codeType;
 	private ArrayList<String> authors;
 	private ArrayList<String> commits;
 
@@ -18,13 +23,12 @@ public class Line implements Serializable {
 	 * it or if has been changed over time
 	 */
 	public Line(ArrayList<String> commits, ArrayList<String> authors, int lineNumber,
-			String lineValue, int changes, String codeType) {
+			String lineValue, int type) {
 		this.authors = authors;
 		this.commits = commits;
 		this.lineNumber = lineNumber;
 		this.lineValue = lineValue;
-		timesChanged = changes;
-		this.codeType = codeType;
+		this.codeType = type;
 	}
 
 	public int getLineNumber() {
@@ -35,18 +39,20 @@ public class Line implements Serializable {
 		return lineValue;
 	}
 
-	public int getTimesChanged() {
-		return timesChanged;
-	}
-
-	public String getCodeType() {
+	public int getCodeType() {
 		return codeType;
 	}
 
+	/*
+	 * Ordered by newest first
+	 */
 	public Iterator<String> authorIterator() {
 		return authors.iterator();
 	}
 
+	/*
+	 * Ordered by newest first
+	 */
 	public Iterator<String> commitIterator() {
 		return commits.iterator();
 	}
