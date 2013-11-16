06829fcfd8e28f75b9ff99aa9789cd69d227d7e5
Justin Chu <justinchu1989@gmail.com>
2013-11-16 11:35:31 -0800

    making needed changes to line object. Repofile is currently broken however.

--- src/data/Line.java
+++ src/data/Line.java
@@ -6,41 +6,25 @@ import java.util.Iterator;
 
 public class Line implements Serializable {
 	
-	private String commitId;
-	private String author;
 	private int lineNumber;
 	private String lineValue;
 	private int timesChanged;
 	private String codeType;
-	
-	//replace "author" and "commitId" with the following:
-	private ArrayList<String> authors; //list of authors in order of last commit
-	private ArrayList<String> commitIds;//corresponding commit ids
-	
-	public Iterator<String> getAuthors() {
-		return authors.iterator();
-	}
+	private ArrayList<String> authors;
+	private ArrayList<String> commits;
 
-	public Iterator<String> getCommitIds() {
-		return commitIds.iterator();
-	}	
-	
-	public Line(String commitId, String author,
-			int lineNumber, String lineValue, int changes, String codeType){
-		this.commitId = commitId;
-		this.author = author;
+	/*
+	 * Represents a single line in a file with information about who generated
+	 * it or if has been changed over time
+	 */
+	public Line(ArrayList<String> commits, ArrayList<String> authors, int lineNumber,
+			String lineValue, int changes, String codeType) {
+		this.authors = authors;
+		this.commits = commits;
 		this.lineNumber = lineNumber;
 		this.lineValue = lineValue;
 		timesChanged = changes;
 		this.codeType = codeType;
-	}
-	
-	public String getCommitId() {
-		return commitId;
-	}
-
-	public String getAuthor() {
-		return author;
 	}
 
 	public int getLineNumber() {
@@ -50,14 +34,20 @@ public class Line implements Serializable {
 	public String getLineValue() {
 		return lineValue;
 	}
-	
+
 	public int getTimesChanged() {
 		return timesChanged;
 	}
-	
-	public String getCodeType()
-	{
+
+	public String getCodeType() {
 		return codeType;
 	}
-	
+
+	public Iterator<String> authorIterator() {
+		return authors.iterator();
+	}
+
+	public Iterator<String> commitIterator() {
+		return commits.iterator();
+	}
 }
